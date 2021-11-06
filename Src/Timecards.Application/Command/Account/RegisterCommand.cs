e92974b3e23f65a7838f95b9c5c5e105c88e8112
yuanjie.liu <>
2021-11-06 10:01:29 +0800

    add the ConfirmPassword property

--- Src/Timecards.Application/Command/Account/RegisterCommand.cs
+++ Src/Timecards.Application/Command/Account/RegisterCommand.cs
@@ -8,6 +8,7 @@ namespace Timecards.Application.Command.Account
         public string UserName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
         public RoleType RoleType { get; set; }
     }
 }
