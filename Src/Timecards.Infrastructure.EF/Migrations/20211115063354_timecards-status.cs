34778111bd5ee9bb4b70109762e76564c71fad24
yuanjie.liu <>
2021-11-15 15:01:44 +0800

    add status

---
+++ Src/Timecards.Infrastructure.EF/Migrations/20211115063354_timecards-status.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Timecards.Infrastructure.EF.Migrations
+{
+    public partial class timecardsstatus : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte>(
+                name: "StatusType",
+                table: "Timecardses",
+                type: "tinyint",
+                nullable: false,
+                defaultValue: (byte)0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StatusType",
+                table: "Timecardses");
+        }
+    }
+}
